6ba1ad1f612ca81e82f116774efa6fc97a19cda7
Dmitri Rubinstein <rubinstein@cs.uni-saarland.de>
2013-03-08 12:53:18 +0100

    Added small node.js server for testing.
    Run with "node startserver.js [PORT]", port is 8080 by default.
    On Linux just run ./startserver.js.

---
+++ startserver.js
@@ -0,0 +1,64 @@
+#! /usr/bin/env node
+
+if (process.argv.length > 2 && process.argv[2] == '--help') {
+  console.log('Usage: node ' + process.argv[1] + ' [PORT]');
+  console.log(' PORT is by default 8080');
+  process.exit(0);
+}
+
+var port = 8080;
+if (process.argv.length > 2) {
+  port = +process.argv[2];
+}
+
+var express;
+try {
+  var express = require('express');
+} catch (err) {
+  console.error("Please install express first: npm install express");
+  console.error(err);
+  process.exit(1);
+}
+var http = require('http');
+
+var server = express();
+
+server.configure(function() {
+  server.set('port', port);
+  server.use(express.favicon());
+  server.use(express.logger('dev'));
+
+  // CORS support
+  server.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
+  // Serve index.xhtml when root is specified '/'
+  server.get('/', function(req, res) {
+    res.sendfile('index.xhtml')
+  });
+
+  server.use(server.router);
+  server.use(express.directory(__dirname)); // serve directories
+  server.use(express.static(__dirname)); // static content
+});
+
+server.configure('development', function(){
+  server.use(express.errorHandler());
+});
+
+// Run server
+
+http.createServer(server).listen(server.get('port'), function() {
+  console.log("Xml3d server listening on http://localhost:" + server.get('port'));
+});
+
+// Local Variables:
+// tab-width:2
+// c-basic-offset: 2
+// espresso-indent-level: 2
+// indent-tabs-mode: nil
+// End:
+// vim: set expandtab tabstop=2 shiftwidth=2:
