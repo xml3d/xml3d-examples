623db5f2e3f2858fe270ab30e001e018bd356465
Kristian Sons <kristian.sons@dfki.de>
2013-04-23 14:22:40 +0200

    Removed ImageGeometry example (for now)

--- script/page.js
+++ script/page.js
@@ -38,9 +38,6 @@ var EXAMPLE_LIST = [
         info: "Demonstrates the usage of emmisive maps for the shading of a flame."},
     {cat: "shader", name: "Custom Shader: Eyelight", href: "examples/eyelight/eyelight.xhtml",
         info: "Demonstrates the usage of custom shaders to implement an eyelight shader."},
-    {cat: "shader", name: "Custom Shader: Image Geometry", href: "examples/imagegeometry/dragon.xhtml",
-        info: "Demonstrates the usage of xflow and custom shaders to implement sequential image geometry (SIG)."},
-
     {cat: "xflow", name: "Sequential Morphing", href: "examples/xflowSequentialMorph/xflow-morph.xhtml",
         info: "Demonstrates mesh morphing with Xflow"},
     {cat: "xflow", name: "Facemorphing", href: "examples/facemorph/facemorph.xhtml",
