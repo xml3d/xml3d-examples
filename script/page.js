43b881c28518d4cd1ded3a0d9e2947530c2e63a4
Kristian Sons <kristian.sons@dfki.de>
2014-10-14 10:25:52 +0200

    Added shade.js example (TV with animated screen)

--- script/page.js
+++ script/page.js
@@ -40,6 +40,9 @@ var EXAMPLE_LIST = [
         info: "Demonstrates the usage of custom shaders to implement an eyelight shader."},
     {cat: "shader", name: "Shader Overrides", href: "examples/shaderOverrides/index.html",
         info: "Demonstrates overriding of shader attributes in the mesh."},
+    {cat: "shader", name: "shade.js: Animated shader", href: "examples/shade-tv/index.html",
+        info: "Animated shader with shade.js."},
+
 	{cat: "xflow", name: "Sequential Morphing", href: "examples/xflowSequentialMorph/xflow-morph.html",
         info: "Demonstrates mesh morphing with Xflow"},
     {cat: "xflow", name: "Facemorphing", href: "examples/facemorph/facemorph.html",
